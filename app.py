5008c1a3464b3c00bdf722c82670017b5cbd31b2
zhuochen <zhuochen777@gmail.com>
2022-10-04 17:12:56 -0700

    add code for updating a todo item complete status

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -8,17 +9,19 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 # link SQLAlchemy to flask app
+migrate = Migrate(app, db)
 
 # create Todo table model
 class Todo(db.Model):
     __tablename__ = "todos"
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable = False)
+    completed = db.Column(db.Boolean, nullable = False, default = False)
 
     def __repr__(self):
         return f"<Todo {self.id} is {self.description}>"
 
-db.create_all()
+# db.create_all()
 
 # create a route that listens request to todos/create with post method
 @app.route("/todos/create", methods=["POST"])
@@ -45,9 +48,22 @@ def create_todo():
         # route handler should always return something or raise an intentional exception
 
 
+@app.route("/todos/<todoId>/set-completed", methods=["POST"])
+def set_completed_todo(todoId):
+    try:
+        completed = request.get_json()["completed"]
+        todo = Todo.query.get(todoId)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for("index"))
+
 @app.route("/")
 def index():
-    return render_template("index.html", data=Todo.query.all())
+    return render_template("index.html", data=Todo.query.order_by("id").all())
 
 
 if __name__=="__main__":
