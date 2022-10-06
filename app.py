eb1c0ca83a0e25f1e81f76011098a9954f4f844b
zhuochen <zhuochen777@gmail.com>
2022-10-06 15:28:07 -0700

    code for creating todo lists

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import json
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
@@ -37,16 +38,20 @@ class TodoList(db.Model):
 #db.create_all()
 
 # create a route that listens request to todos/create with post method
+# create todo item
 @app.route("/todos/create", methods=["POST"])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()["description"]
-        todo = Todo(description = description)
+        list_id = request.get_json()["list_id"]
+        todo = Todo(description = description, completed = False, list_id = list_id)
         db.session.add(todo)
         db.session.commit()
         body["description"] = todo.description
+        body["completed"] = todo.completed
+        body["list_id"] = todo.list_id
     except:
         error = True
         db.session.rollback()
@@ -60,7 +65,7 @@ def create_todo():
         # rise an HTTPException for an Internal Server Error
         # route handler should always return something or raise an intentional exception
 
-
+# update todo item
 @app.route("/todos/<todoId>/set-completed", methods=["POST"])
 def set_completed_todo(todoId):
     try:
@@ -74,6 +79,7 @@ def set_completed_todo(todoId):
         db.session.close()
     return redirect(url_for("index"))
 
+# delete todo item
 @app.route("/todos/<todoId>/delete", methods=["DELETE"])
 def delete_todo(todoId):
     try:
@@ -86,6 +92,12 @@ def delete_todo(todoId):
         db.session.close()
     return jsonify({"success": True})
 
+# home route
+@app.route("/")
+def index():
+    return redirect(url_for("get_list_todo", list_id = 1))
+
+# default route
 @app.route("/lists/<list_id>")
 def get_list_todo(list_id):
     return render_template("index.html",
@@ -94,10 +106,22 @@ def get_list_todo(list_id):
     todos = Todo.query.filter_by(list_id = list_id).order_by("id").all())
     # order_by("id") so that records are listed by id even after webpage refreshes
 
+# create list item
+@app.route("/lists/create", methods=["POST"])
+def create_list():
+    body = {}
+    try:
+        listname = request.get_json()["name"]
+        list = TodoList(name = listname)
+        db.session.add(list)
+        db.session.commit()
+        body["name"] = list.name
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+        return jsonify(body)
 
-@app.route("/")
-def index():
-    return redirect(url_for("get_list_todo", list_id = 1))
 
 if __name__=="__main__":
     app.run(debug=True)
